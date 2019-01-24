4be56228b63a927f822a62c77655e5f7665c715a
lindaAcc <lbenboudiaf@gmail.com>
2019-01-24 02:17:12 +0100

    Recalculate board on space

--- CGAL3D/Plateau.py
+++ CGAL3D/Plateau.py
@@ -5,65 +5,72 @@ from math import *
 
 # Mettre en place Camèra + Lumière ...
 # Scene represente le monde "World" on definit certain paramètre initial... 
-scene = display(x=0, y=0, z=0, width= 800, height=900, center=(1,0,0),title = 'Plateau.Inside', background=(1, 1, 1))
+scene = display(x=0, y=0, title = 'Plateau.Inside', background=(1, 1, 1))
 scene.range = 5
 scene.autocenter = True
 # Lumière 
 local_light(pos=(-10,-10,10), color=color.yellow)
-
+TailleCube = vector(2,2,2)
 #Premier niveau un pion possible 
-b1= box(pos=(0, 4,2), size=(2,2,2))
-b1Bis= box(pos=(0,4,0), size=(2,2,2))
-b2= box (pos=(2,2,2), size=(2,2,2))
-b2Bis= box (pos=(2,2,0), size=(2,2,2))
-b3= box(pos=(0,2,2), size=(2,2,2)) # Pas de cube jumelle 
+b= box(pos=(0, -4,2), size= TailleCube) 
+b= box(pos=(0,-4,0), size=TailleCube)
+b= box (pos=(-2,-2,2), size=TailleCube)
+b= box (pos=(-2,-2,0), size=TailleCube)
+b= box(pos=(0,-2,2), size=TailleCube) # Pas de cube jumelle 
 #Deuxieme Niveau 
-b4= box(pos=(0,6,0), size=(2,2,2))
-b4Bis = box(pos=(0,6,-2), size=(2,2,2))
-b5= box(pos=(2,4,0), size=(2,2,2))
-b5Bis = box(pos=(2,4,-2), size=(2,2,2))
-b6= box(pos=(4,2,0), size=(2,2,2))
-b6Bis= box(pos=(4,2,-2), size=(2,2,2))
+b= box(pos=(0,-6,0), size=TailleCube)
+b= box(pos=(0,-6,-2), size=TailleCube)
+b= box(pos=(-2,-4,0), size=TailleCube)
+b= box(pos=(-2,-4,-2), size=TailleCube)
+b= box(pos=(-4,-2,0), size=TailleCube)
+b= box(pos=(-4,-2,-2), size=TailleCube)
 #Vu arrière du plateau #Troisième niveau
-b7= box(pos=(0,8,-2), size=(2,2,2))
-b7Bis= box(pos=(0,8,-4), size=(2,2,2))
-b8= box(pos=(2,6,-2), size=(2,2,2))
-b8Bis= box(pos=(2,6,-4), size=(2,2,2))
-b9= box(pos=(4,4,-2), size=(2,2,2))
-b9Bis= box(pos=(4,4,-4), size=(2,2,2))
-b10= box(pos=(6,2,-2), size=(2,2,2))
-b10Bis = box(pos=(6,2,-4), size=(2,2,2))
+b= box(pos=(0,-8,-2), size=TailleCube)
+b= box(pos=(0,-8,-4), size=TailleCube)
+b= box(pos=(-2,-6,-2), size=TailleCube)
+b= box(pos=(-2,-6,-4), size=TailleCube)
+b= box(pos=(-4,-4,-2), size=TailleCube)
+b= box(pos=(-4,-4,-4), size=TailleCube)
+b= box(pos=(-6,-2,-2), size=TailleCube)
+b= box(pos=(-6,-2,-4), size=TailleCube)
 # Quatième Niveau vu arrière 
-b11= box(pos=(0,10,-4),size=(2,2,2))
-b11Bis = box(pos=(0,10,-6),size=(2,2,2))
-b12= box(pos=(2,8,-4), size=(2,2,2))
-b12Bis= box(pos=(2,8,-6), size=(2,2,2))
-b13= box(pos=(4,6,-4), size=(2,2,2))
-b13Bis= box(pos=(4,6,-6), size=(2,2,2))
-b14= box(pos=(6,4,-4), size=(2,2,2))
-b14Bis= box(pos=(6,4,-6), size=(2,2,2))
-b15= box(pos=(8,2,-4), size=(2,2,2))
-b15Bis= box(pos=(8,2,-6), size=(2,2,2))
+b= box(pos=(0,-10,-4),size=TailleCube)
+b= box(pos=(0,-10,-6),size=TailleCube)
+b= box(pos=(-2,-8,-4), size=TailleCube)
+b= box(pos=(-2,-8,-6), size=TailleCube)
+b= box(pos=(-4,-6,-4), size=TailleCube)
+b= box(pos=(-4,-6,-6), size=TailleCube)
+b= box(pos=(-6,-4,-4), size=TailleCube)
+b= box(pos=(-6,-4,-6), size=TailleCube)
+b= box(pos=(-8,-2,-4), size=TailleCube)
+b= box(pos=(-8,-2,-6), size=TailleCube)
 # Dernier niveau
-b16= box(pos=(0,12,-6), size=(2,2,2))
-b17= box(pos=(2,10,-6), size=(2,2,2))
-b18= box(pos=(4,8,-6), size=(2,2,2))
-b19= box(pos=(6,6,-6), size=(2,2,2))
-b20= box(pos=(8,4,-6), size=(2,2,2))
-b21= box(pos=(10,2,-6), size=(2,2,2))
+b= box(pos=(0,-12,-6), size=TailleCube)
+b= box(pos=(-2,-10,-6), size=TailleCube)
+b= box(pos=(-4,-8,-6), size=TailleCube)
+b= box(pos=(-6,-6,-6), size=TailleCube)
+b= box(pos=(-8,-4,-6), size=TailleCube)
+b= box(pos=(-10,-2,-6), size=TailleCube)
 
 # Pied du Plateau ...
-b22= box(pos=(0,12,-8), size=(2,2,2))
-b22Bis= box(pos=(2,12,-8), size=(2,2,2))
-b23= box(pos=(4,10,-8), size=(2,2,2))
-b23Bis= box(pos=(2,10,-8), size=(2,2,2))
-b24= box(pos=(6,8,-8), size=(2,2,2))
-b24Bis= box(pos=(4,8,-8), size=(2,2,2))
-b25= box(pos=(8,6,-8), size=(2,2,2))
-b25Bis= box(pos=(6,6,-8), size=(2,2,2))
-b26= box(pos=(10,4,-8), size=(2,2,2))
-b26Bis= box(pos=(8,4,-8), size=(2,2,2))
-b27= box(pos=(10,2,-8), size=(2,2,2))
+b= box(pos=(0,-12,-8), size=TailleCube)
+b= box(pos=(-2,-12,-8), size=TailleCube)
+b= box(pos=(-4,-10,-8), size=TailleCube)
+b= box(pos=(-2,-10,-8), size=TailleCube)
+b= box(pos=(-6,-8,-8), size=TailleCube)
+b= box(pos=(-4,-8,-8), size=TailleCube)
+b= box(pos=(-8,-6,-8), size=TailleCube)
+b= box(pos=(-6,-6,-8), size=TailleCube)
+b= box(pos=(-10,-4,-8), size=TailleCube)
+b= box(pos=(-8,-4,-8), size=TailleCube)
+b= box(pos=(-10,-2,-8), size=TailleCube)
+
+board = []
+for i in range(48):
+        board.append(b)
+        label (pos=(0,0,0), text='This is a Cube')
+        
+
 
 # Définition de la couleur du plateau (Des cubes quoi !!)
 CouleurChoco = vector(86.7,39.26,17.69)
@@ -72,11 +79,7 @@ for cube in scene.objects:
         cube.color = vector(1,0.5, 1)
         
 # Drag and Drop du Cube
-cube = box(pos=(10, -10,0), size=(2,2,2),color= color.yellow)
-n = 18
-#for i in range(n):
-    #cube = box(pos=(0, 0,0), size=(2,2,2),color= color.yellow)
-
+cube = box(pos=(0, -2, 8), size=TailleCube,color= color.yellow)
 drag_pos = None
 def take_obj(event):
     global drag_pos
@@ -96,3 +99,19 @@ def drop(evt):
     scene.unbind('mouseup', drop)
 
 scene.bind('mousedown', take_obj)
+
+
+## Créer des cubes pour les joueur 18 pour chaqu'un + attribution de couleurs ...
+#def jeu(): 
+##       nb_cubesJ1 = 18
+#        nb_cubesJ2 = 18 # pour la machine du coup  
+#        cubes_j1=[]
+#        cubes_j2= []
+#        for i in range(nb_cubesJ1):
+#                BJ1 = box(size=TailleCube, color= color.cyan)
+#                cubes_j1.append(BJ1)
+#
+#        for i in range(nb_cubesJ2):
+#                BJ2 = box (size=TailleCube, color= color.green)
+ #               cubes_j2.append(BJ2)
+#jeu()
