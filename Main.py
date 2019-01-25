e5cd51a40c0b4fcd46e05063f39cefae9125d196
anozername <anozername@protonmail.com>
2019-01-25 04:20:06 +0100

    select improved

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ from Game import *
 def main():
     print(sys.version)
     #py = Pyramide.Pyramide()
-    j1 = Joueur.Joueur(1, "j1")
-    j2 = Joueur.Joueur(2, "j2")
+    j1 = Joueur.Joueur(1, "j1", 'fill')
+    j2 = Joueur.Joueur(2, "j2", 'fill')
     jeu = Jeu.Jeu(j1, j2)
     Plateau0.main(jeu)
     Plateau0.parcours()
